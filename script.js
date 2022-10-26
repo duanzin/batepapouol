59b59e7a8d3c49a4ca66c1f2b5d4ea6b507d9d86
duanzin <lucaduan@gmail.com>
2022-10-26 20:41:01 -0300

    puxa mensagens da api

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
-//<li>(09:21:45)  João  entra na sala...</li>
-let chat = {
-    nome: usuario,
-    para: destinatario,
-    tipo: classe,
-    mensagem: texto,
-    hora: data
+let usuario = prompt("digite seu nome");
+
+//array com as informaçoes do api
+let info = []; 
+const promessa = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+promessa.then(respostaChegou);
+
+function respostaChegou(resposta){
+    info = resposta.data;
+    
+    //imprime as mensagens
+    rederizarmensagens();
 }
+function rederizarmensagens(){
+
+    const chat = document.querySelector('ul');
+
+    chat.innerHTML = '';
+    
+    for(let i =0; i < info.length; i++){
+
+        let mensagem = info[i];
+
+        switch(mensagem.type){
+            case 'message':
+            chat.innerHTML += `
+                <li>
+                    ${mensagem.time} ${mensagem.from} para ${mensagem.to}: ${mensagem.text}
+                </li>`;
+            break;
+            case 'status':
+            chat.innerHTML += `
+                <li class = "status">
+                    ${mensagem.time} ${mensagem.from} ${mensagem.text}
+                </li>`;
+            break;
+            case 'private_message':
+            chat.innerHTML += `
+                <li class = "reservada">
+                    ${mensagem.time} ${mensagem.from} reservadamente para ${mensagem.to}: ${mensagem.text}
+                </li>`;
+            break;
+        }
+    }
+
+}
+rederizarmensagens();
